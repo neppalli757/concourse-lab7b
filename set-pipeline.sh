3e95319a58685c635e821ff2ba43aa63e5e37c40
skybot757 <sravan.neppalli@cgi.com>
2017-06-29 11:02:18 -0600

    remove tab

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -2,24 +2,18 @@
 
 set -ex
 
-if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ] || [ "$5" = "" ]; then
-  echo $0: usage: $0 concourse-target pipeline.yml config.yml credentials.yml pipeline-name
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ]; then
+  echo $0: usage: $0 concourse-target pipeline.yml  credentials.yml pipeline-name
   exit
 fi
 
 concourse_target=$1
 pipeline=$2
-config=$3
-credentials=$4
-pipeline_name=$5
+credentials=$3
+pipeline_name=$4
 
 if [ ! -e  $pipeline ]; then
   echo $0: Pipeline file does not exist: $pipeline
-  exit
-fi
-
-if [ ! -e  $config ]; then
-  echo $0: Config file does not exist: $config
   exit
 fi
 
@@ -29,7 +23,7 @@ if [ ! -e  $credentials ]; then
 fi
 
 cat $credentials > temp-config.yml
-cat $config >> temp-config.yml
+
 
 fly -t $concourse_target set-pipeline -c $pipeline -l temp-config.yml -p $pipeline_name -n
 
